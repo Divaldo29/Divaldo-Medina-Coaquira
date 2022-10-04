2a567d1842f4a2c1088098dddbbc7b8287b3130e
Divaldo29 <divaldocoaquira@gmail.com>
2022-10-03 20:11:10 -0500

    Examen Divaldo

---
+++ src/test/java/Divaldo/Medina/DMedinaaApplicationTests.java
@@ -0,0 +1,13 @@
+package Divaldo.Medina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DMedinaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
